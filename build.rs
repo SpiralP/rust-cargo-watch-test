434757ec1595b858025ba4165469fc5e4acd2ea0
SpiralP <spiral.palmer@gmail.com>
2019-03-01 16:55:20 -0800

    add a sleep after touching the ignored file for the watcher to see the change

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 use std::io::Write;
 use std::fs;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let mut f = fs::File::create("ignored_folder/file.txt").unwrap();
-    f.write_all(b"hello").unwrap();
+    {
+        let mut f = fs::File::create("ignored_folder/file.txt").unwrap();
+        f.write_all(b"hello").unwrap();
+    }
+
+    sleep(Duration::from_millis(1000));
 }
